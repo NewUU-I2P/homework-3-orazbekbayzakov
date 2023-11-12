737c573816b47c6d3f3623cbe17b87fb1bd88a2e
orazbekbayzakov <147370780+orazbekbayzakov@users.noreply.github.com>
2023-11-12 19:59:14 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,20 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::string result;
+    std::string firstOctet = macAddress.substr(0, 2);
+    int firstOctetValue = stoi(firstOctet,nullptr, 16);
 
+    if (firstOctetValue % 2 == 0) {
+        result = "Unicast";
+    }
+    else if (firstOctetValue % 2 != 0) {
+        result = "Multicast";
+    }
+    if (macAddress == "FF:FF:FF:FF:FF:FF") {
+        result = "Boardcast";
+    }
+    return result;
     // make use of control flow statements
     // return result;
 }
