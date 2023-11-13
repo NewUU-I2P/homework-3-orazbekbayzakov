1547129d3a95c52038410175bc20049391617386
orazbekbayzakov <147370780+orazbekbayzakov@users.noreply.github.com>
2023-11-13 10:30:13 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,13 +2,13 @@ float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
     if(consumed_water <= 30.0){
-        cost = float(13 + consumed_water * 0.4);
+        cost = 13.0 + consumed_water * 0.4;
     }else if(consumed_water <= 50.0){
-        cost = float(13 + 30 * 0.4 + (consumed_water - 30) * 0.12);
+        cost = 13.0 + 30.0 * 0.4 + (consumed_water - 30.0) * 0.12;
     }else if(consumed_water <= 60.0) {
-        cost = float(13 + 30 * 0.4 + 20 * 0.12 + (consumed_water - 50) * 1.4);
+        cost = 13.0 + 30.0 * 0.4 + 20.0 * 0.12 + (consumed_water - 50.0) * 1.4;
     }else{
-        cost = float(13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5);
+        cost = 13.0 + 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumed_water - 60.0) * 1.5;
     }
     return cost;
 }
