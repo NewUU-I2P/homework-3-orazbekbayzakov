58e736eb0a0e1493c7d38f50e61e68f60949feb7
orazbekbayzakov <147370780+orazbekbayzakov@users.noreply.github.com>
2023-11-13 11:56:04 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,4 +1,4 @@
-void problemSolution1(float consumed_water) {
+float problemSolution1(float consumed_water) {
     float cost = 13.0;
     // write your code here
     if(consumed_water <= 30.0){
@@ -10,4 +10,5 @@ void problemSolution1(float consumed_water) {
     }else{
         cost = cost + 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumed_water - 60.0) * 1.5;
     }
+    return cost;
 }
